1c0a08350aec62ab147960b6a1a777f0d28d0688
EC2 Default User <ec2-user@ip-172-31-86-29.ec2.internal>
2019-12-20 06:12:19 +0000

    follow unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
     root to: "toppages#index"
+    
     get 'signup', to: 'users#new'
-    resources :users, only: [:index, :show, :new, :create]
+    
     get 'login', to: 'sessions#new'
     post 'login', to: 'sessions#create'
     delete 'logout', to: 'sessions#destroy'
+    
     resources :microposts, only: [:create, :destroy]
+    resources :users, only: [:index, :show, :new, :create] do
+        member do
+            get :followings
+            get :followers
+        end
+    end
+    resources :relationships, only: [:create, :destroy]
 
 end
